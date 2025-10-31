0ee219be05a430c18af85d4c9de31c223fad6251
Nicholas Cioli <nicholascioli@users.noreply.github.com>
2025-10-31 15:43:54 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Ambros Bandoneon Method written as LaTeX with Lilypond";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    nixpkgs,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          lilypond
+          tetex
+        ];
+      };
+    });
+}
