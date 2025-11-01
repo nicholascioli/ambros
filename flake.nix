c2729156186f3d23379db6aa5a0cfd0d5fcde3d2
Nicholas Cioli <nicholascioli@users.noreply.github.com>
2025-11-01 07:11:09 -0400

    prepare for CI

--- flake.nix
+++ flake.nix
@@ -3,21 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
+
     flake-utils.url = "github:numtide/flake-utils";
+    nix-filter.url = "github:numtide/nix-filter";
   };
 
   outputs = {
     self,
     flake-utils,
+    nix-filter,
     nixpkgs,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
+      filter = nix-filter.lib;
+
+      tex = pkgs.texlive.combine {
+        inherit (pkgs.texlive) scheme-minimal latex-bin latexmk titling;
+      };
     in {
       devShells.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           lilypond
-          tetex
+          tex
         ];
       };
     });
